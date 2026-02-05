f0843e642714b310e6cc011f5a776c84272b551e
Foleiz <follz7700@gmail.com>
2026-02-05 23:53:19 +0700

    last9

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import History from "./pages/History";
 import "./styles.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <nav>
+          <Link to="/">หน้าแรก</Link>
+          <Link to="/history">ประวัติ</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
